6ea13f53aa9ad334e63007e597e6f72b65b1c151
Lejla Kusari <kusarilejla@gmail.com>
2018-11-12 16:04:39 +0100

    Add RiskType model and register the model in admin.

--- risks/models.py
+++ risks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class RiskType(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(RiskType, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return 'Risk type: {}'.format(self.name)
