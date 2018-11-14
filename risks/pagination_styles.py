afa6f424865b9ffc569b5bab179f43ebba3f8fdb
Lejla Kusari <kusarilejla@gmail.com>
2018-11-14 12:28:04 +0100

    Update gitignore and add extra pagination style, just in case of need.

--- risks/pagination_styles.py
+++ risks/pagination_styles.py
@@ -5,3 +5,9 @@ class StandardResultsPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 15
+
+
+class LargeResultsPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = 'page_size'
+    max_page_size = 100
